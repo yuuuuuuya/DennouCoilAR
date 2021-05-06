4dc9e9e956c8603653210342335b4b66762c79e2
伊島悠矢 <b.ald.m.wn@gmail.com>
2021-05-06 15:59:08 +0900

    first commit

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Portal : MonoBehaviour
+{
+    static bool enteredOldSpace = false; // oldSpaceに入っている→true
+    [SerializeField] Camera portalCam;
+    [SerializeField] Camera PortalExitCam;
+    [SerializeField] GameObject PortalFace;
+    [SerializeField] GameObject MaskFace;
+    Camera mainCam;
+    PostProcessLayer mainCamPPLayer;
+    PostProcessLayer portalCamPPLayer;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+        mainCamPPLayer = mainCam.GetComponent<PostProcessLayer>();
+        portalCamPPLayer = portalCam.GetComponent<PostProcessLayer>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera") && GameController.portalFaceState == 2)
+        {
+            //portalからカメラへの方向ベクトル
+            Vector3 dictance = transform.InverseTransformDirection(other.transform.position - transform.position);
+
+            // portalの正面方向から衝突時のみ
+            if (dictance.z < 0)
+            {
+                // メインカメラで、OldSpaceレイヤーの表示・非表示切替
+                mainCam.cullingMask ^= 1 << LayerMask.NameToLayer("OldSpace");
+                mainCamPPLayer.volumeLayer ^= 1 << LayerMask.NameToLayer("OldSpace");
+
+                // サブカメラで、OldSpaceレイヤーの表示・非表示切替
+                portalCam.cullingMask ^= 1 << LayerMask.NameToLayer("OldSpace");
+                portalCamPPLayer.volumeLayer ^= 1 << LayerMask.NameToLayer("OldSpace");
+
+                // portalFaceとMaskFaceの切替
+                if (enteredOldSpace)
+                {
+                    PortalExitCam.enabled = false;
+                    PortalFace.SetActive(true);
+                    MaskFace.SetActive(false);
+                    enteredOldSpace = false;
+                }
+                else
+                {
+                    PortalExitCam.enabled = true;
+                    PortalFace.SetActive(false);
+                    MaskFace.SetActive(true);
+                    enteredOldSpace = true;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# DennouCoil
+
+電脳コイル(アニメ)の世界をイメージ。<br>
+メタバグを集めたり、古い空間に入ったりする事が可能<br>
+
+# DEMO
+
+[![demo](https://github.com/yuuuuuuya/DennouCoil/wiki/images/DennouCoil.gif)](https://github.com/yuuuuuuya/DennouCoil/wiki/images/DennouCoil.gif)
+
+# Usage
+
+黒い壁を数回タッチ→古い空間へのゲートが登場<br>
+メタバグをシングルタッチ→取得<br>
+画面を長押し→ウィンドウの表示・非表示切替。ウィンドウには集めたメタバグの量が表示
+
+# Author
+
+* 伊島悠矢
+* b.ald.m.wn@gmail.com
